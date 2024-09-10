93b954d86e13df4accb3adad7e82048c17300e5d
AGBR121 <angel.burbano@utp.edu.co>
2024-09-10 12:07:29 -0500

    Enter the name of the players

--- memorama.c
+++ memorama.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
-char* CreateString(char cadena[], int counter, int number){
-    if(counter < number){
-        cadena[counter]= ' ';
-        
+char* CreateString(char cadena[], int counter, int number, char letter) {
+    if (counter < number) {
+        cadena[counter] = letter;
+        return CreateString(cadena, counter + 1, number, letter);
+    } else {
+        cadena[counter] = '\0'; 
+        return cadena;
     }
 }
 
@@ -43,19 +47,27 @@ int main() {
 
     char cadena[] = "aaaaaaaaaaaaaaaa";  
     char* game = GenerateCadena(cadena, 0, 0, 'A');
-    char player1[] = "";
-    char player2[] = "";
+    char player1[50];
+    char player2[50];
+    
     printf("\nEntre el nombre del jugador 1: ");
-    scanf("%s", player1);
+    scanf("%49s", player1); 
+    
     printf("\nEntre el nombre del jugador 2: ");
-    scanf("%s", player2);
+    scanf("%49s", player2);
+    
     int longitud1 = strlen(player1);
     int longitud2 = strlen(player2);
-    int espacios = strlen("                     ");
+    int nEspacios = 12; 
+    
+    char espacios1[50]; 
+    char espacios2[50]; 
+    CreateString(espacios1, 0, nEspacios - longitud1, ' ');
+    CreateString(espacios2, 0, nEspacios - longitud2, ' ');
+
     printf("\n%s\n", game);
-    printf("+---+---+---+---+  JUEGO DEL CONCENTRESE        SCORE\n| 1 | 2 | 3 | 4 |  JUGADOR 1:                     0\n+---+---+---+---+  JUGADOR 2:                     0\n");
+    printf("+---+---+---+---+  JUEGO DEL CONCENTRESE        SCORE\n| 1 | 2 | 3 | 4 |  Nombre jugador 01: %s%s0\n+---+---+---+---+  Nombre jugador 02: %s%s0\n", player1, espacios1, player2, espacios2);
     printf("| 5 | 6 | 7 | 8 |\n+---+---+---+---+\n| 9 | 10| 11| 12|\n+---+---+---+---+\n| 13| 14| 15| 16|\n+---+---+---+---+\n");
-    //Game(1, game);
     
     return 0;
 }
