418b4a48853d8e5d2e6b8cc6d26a7a7510b910eb
AGBR121 <angel.burbano@utp.edu.co>
2024-09-27 10:43:41 -0500

    print arreglado sin logica

--- memorama.c
+++ memorama.c
@@ -16,15 +16,6 @@ char* CreateString(char cadena[], int counter, int number, char letter) {
     }
 }
 
-int EndGame(const char* str, int pos) {
-    if (pos == 64) {
-        return 1;  
-    }
-    if (isdigit(str[pos])) {
-        return 0; 
-    }
-    return EndGame(str, pos + 1);
-}
 
 char* GenerateCadena(char cadena[], int counter, int counter2, int letter) {
     int num = rand() % 16;  
@@ -49,13 +40,6 @@ char* GenerateCadena(char cadena[], int counter, int counter2, int letter) {
     }
 }
 
-int Desicion(char option) {
-    if (option == 's' || option == 'S') {
-        return 1; 
-    } else {
-        return 0;
-    }
-}
 
 void ChangeChar(char str[], int pos, char newChar) {
     if (pos < 9) {
@@ -76,81 +60,17 @@ void PrintTable(char table[], char player1[], char player2[], char player[], cha
     char espacios2[50];
 
     CreateString(espacios1, 0, nEspacios - longitud1, ' '); 
-    CreateString(espacios2, 0, nEspacios - longitud2, ' '); 
-
+    CreateString(espacios2, 0, nEspacios - longitud2, ' ');
     printf("\n+---+---+---+---+  JUEGO DEL CONCENTRESE        SCORE\n");
-    printf("| %c | %c | %c | %c |  Nombre jugador 01: %s%s0\n", &table[0], &table[1], &table[2], &table[3], player1, espacios1);
+    printf("| %c | %c | %c | %c |  Nombre jugador 01: %s%s0\n", table[0], table[1], table[2], table[3], player1, espacios1);
     printf("+---+---+---+---+  Nombre jugador 02: %s%s0\n", player2, espacios2);
-    printf("| %c | %c | %c | %c |\n", &table[4], &table[5], &table[6], &table[7]);
+    printf("| %c | %c | %c | %c |\n", table[4], table[5], table[6], table[7]);
     printf("+---+---+---+---+  Juega: %s\n", player);
-    printf("| %c |%c%c |%c%c |%c%c |  Status: %s\n", &table[8], &table[9], &table[10], &table[11], &table[12], &table[13], &table[14], &table[15], status);
+    printf("| %c |%c%c |%c%c |%c%c |  Status: %s\n", table[8], table[9], table[10], table[11], table[12], table[13], table[14], status);
     printf("+---+---+---+---+\n");
-    printf("|%c%c |%c%c |%c%c |%c%c |\n", &table[16], &table[17], &table[18], &table[19], &table[20], &table[21], &table[22], &table[23]);
+    printf("|%c%c |%c%c |%c%c |%c%c |\n", table[15], table[16], table[17], table[18], table[19], table[20], table[21], table[22]);
     printf("+---+---+---+---+\n");
 }
-
-
-
-void CheckMove(int turn,char newTable[], char stringTable[], char initialTable[], char actualTable[], char player1[], char player2[], int score1, int score2, int selection, int counter, int position, int selection1) {
-    if (isdigit(actualTable[position])) {
-        if (newTable[position] == newTable[selection1]) {
-            PrintTable(newTable, player1, player2, (turn == 1) ? player1 : player2, 
-                       (turn == 1) ? "1" : "0", (turn == 2) ? "1" : "0", 
-                       (score1 + score2 == 7) ? ((turn == 1 && score2 == 4) || (turn == 2 && score1 == 4)) ? 
-                       "Fin del juego empatados" : "Has Ganado!" : "Adivinó");
-            if (EndGame(newTable, 0)) {
-                return;
-            } else {
-                printf("\n%s\n", newTable);
-                Game(turn, stringTable, newTable, newTable, player1, player2, 
-                     (turn == 1) ? score1 + 1 : score1, (turn == 2) ? score2 + 1 : score2, 0, 1);
-            }
-        } else {
-            if (counter == 2) {
-                PrintTable(newTable, player1, player2, (turn == 1) ? player1 : player2, "0", "0", "Perdió");
-                printf("Desea continuar (s/n): ");
-                char option;
-                scanf(" %c", &option);
-                if (Desicion(option)) {
-                    PrintTable(newTable, player1, player2, (turn == 1) ? player1 : player2, "0", "0", " ");
-                    Game(turn == 1 ? 2 : 1, stringTable, newTable, actualTable, player1, player2, score1, score2, 0, 1);
-                }
-            } else {
-                PrintTable(newTable, player1, player2, (turn == 1) ? player1 : player2, "0", "0", " ");
-                Game(turn, stringTable, newTable, newTable, player1, player2, score1, score2, position, counter + 1);
-            }
-        }
-    } else {
-        if (EndGame(actualTable, 0)) {
-            return;
-        } else {
-            printf("Error, casilla ya jugada\n");
-            PrintTable(newTable, player1, player2, (turn == 1) ? player1 : player2, "0", "0", " ");
-            Game(turn, stringTable, newTable, newTable, player1, player2, score1, score2, selection1, counter);
-        }
-    }
-}
-
-void Game(int turn, char stringTable[], char initialTable[], char actualTable[], char player1[], char player2[], int score1, int score2, int selection, int counter) {
-    char newTable[100];
-    int position = 0;
-    printf("Entre número casilla 0%i: ", turn);
-    scanf("%i", &position);
-    strcpy(newTable, actualTable);
-    if (isdigit(actualTable[position])) {
-        char newChar = stringTable[position];
-        printf("\n%i\n%c\n%i", position, newChar, position);
-        printf("\n%s\n", stringTable);
-        ChangeChar(newTable, position, newChar);
-    }
-    if(position > 0 &&position < 17){
-            CheckMove(turn, newTable, stringTable, initialTable, actualTable, player1, player2, score1, score2, selection, counter, position, selection);
-    }else{
-        printf("\nSelección Invalida\n");
-        Game(turn, stringTable, initialTable, actualTable, player1, player2, score1, score2, selection, counter);
-    }
-}
-
 
 int main() {
     srand(time(NULL));
@@ -168,7 +88,7 @@ int main() {
     
     int longitud1 = strlen(player1);
     int longitud2 = strlen(player2);
-    int nEspacios = 12; 
+    int nEspacios = 16; 
     
     char espacios1[50]; 
     char espacios2[50]; 
@@ -176,9 +96,7 @@ int main() {
     CreateString(espacios2, 0, nEspacios - longitud2, ' ');
 
     printf("\n%s\n", game);
-    printf("+---+---+---+---+  JUEGO DEL CONCENTRESE        SCORE\n| 1 | 2 | 3 | 4 |  Nombre jugador 01: %s%s0\n+---+---+---+---+  Nombre jugador 02: %s%s0\n", player1, espacios1, player2, espacios2);
-    printf("| 5 | 6 | 7 | 8 |\n+---+---+---+---+  Juega: %s\n| 9 |10 |11 |12 |  Status:\n+---+---+---+---+\n|13 |14 |15 |16 |\n+---+---+---+---+\n", player1);
-    
-    Game(1, game, initialTable, initialTable, player1, player2, 0, 0, 0, 1);
+    PrintTable(initialTable, player1, player2, player1, "", "", "");
+    //Game(1, game, initialTable, initialTable, player1, player2, 0, 0, 0, 1);
     return 0;
 }
